b81accbedd91f317b8024a902aedca3360e362da
Cauã Almeida <cauaapa@gmail.com>
2022-08-19 20:37:01 -0300

    Update rock_paper_scisors.py

--- 3 - simple/rock_paper_scisors.py
+++ 3 - simple/rock_paper_scisors.py
@@ -13,28 +13,21 @@ def main():
     z = options[x]
     if y == z:
         return "It's a Tie!"
-        main()
     elif y == "rock":
         if z == "paper":
             return "AI has chosen paper. You lose!"
-            main()
         else:
             return "AI has chosen scisors. You win!"
-            main()
     elif y == "paper":
         if z == "scisors":
             return "AI has chosen scisors. You lose!"
-            main()
         else:
             return "AI has chosen rock. You win!"
-            main()
     elif y == "scisors":
         if z == "rock":
             return "AI has chosen rock. You lose!"
-            main()
         else:
             return "AI has chosen paper. You win!"
-            main()
     else:
         return "Invalid input!"
 
